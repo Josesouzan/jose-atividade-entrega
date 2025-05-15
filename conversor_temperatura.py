81f2856f0201e584e8e64c1c8f02642a3e0eb39f
Josesouzan <jzeca85@gmail.com>
2025-05-15 15:42:39 -0300

    Add files via upload

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -6,11 +6,11 @@ while opcao !=0:
     print("0 ----Para sair. ")
     opcao= int(input("escolha uma opação: "))
     if opcao ==1:
-        temperaturainicial=float(input("Informe a temperatura a ser convertida: ")) 
+        temperaturainicial=float(input("Informe a temperatura a ser convertida: ")) #variavel temperatura sendo atribuido valores 
         celsiusfahrenheit= (temperaturainicial*9/5)+32
         print(f"a temperatura é: {celsiusfahrenheit}") 
     elif opcao==2:
-        temperaturainicial=float(input("Informe a temperatura a ser convertida: "))
+        temperaturainicial=float(input("Informe a temperatura a ser convertida: "))#variavel temperatura sendo atribuido valores 
         celsiuskelvin=temperaturainicial+273.15
         print(f"A temperatura é: {celsiuskelvin} ")
     else:
