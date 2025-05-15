a3bda33781cd86be48288b68be9c5fa59a61025f
Josesouzan <jzeca85@gmail.com>
2025-05-15 15:40:49 -0300

    Add files via upload

---
+++ conversor_temperatura.py
@@ -0,0 +1,18 @@
+opcao = int
+while opcao !=0:
+    print("########VAMOS CONVERTER A TEMPERATURA######## ")
+    print("1 ---- Para converter Celsius em Fahrenheit:")
+    print("2 ---- Para converter Celsius em Kelvin: ")
+    print("0 ----Para sair. ")
+    opcao= int(input("escolha uma opação: "))
+    if opcao ==1:
+        temperaturainicial=float(input("Informe a temperatura a ser convertida: ")) 
+        celsiusfahrenheit= (temperaturainicial*9/5)+32
+        print(f"a temperatura é: {celsiusfahrenheit}") 
+    elif opcao==2:
+        temperaturainicial=float(input("Informe a temperatura a ser convertida: "))
+        celsiuskelvin=temperaturainicial+273.15
+        print(f"A temperatura é: {celsiuskelvin} ")
+    else:
+        print("ERRO DE DIGITAÇÃO TENTE NOVAMENTE: ")        
+
